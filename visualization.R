9a1152825cdbb47d186c40b9b9052cef0e36b9c1
frank@epimacpro <zhangc518@gmail.com>
2019-10-08 15:40:26 -0700

    also plot code

--- visualization.R
+++ visualization.R
@@ -1,5 +1,6 @@
 a <- readRDS('./db/tfclass.rds')
-
+a$class <- rbind(a$class,data.frame(stringsAsFactors = F,id="2.7",name='C2HC',about="The known transcription factors exhibiting a DNA-binding domain of intertwined zinc fingers belong to the DM group of proteins, named after Drosophila Doublesex and C. elegans Meb-3. Intertwined are one zinc finger of C2HC and one of HCC2 type, each binding one zinc ion. Together, they follow the consensus pattern C--C--h--------H---C----c-c-c, where upper and lower case letter refer to either zinc finger, and dashes symbolize anyother amino acid. The most C-terminal cysteine is part of a short alpha-helical structure, which extends into a carboxy-terminal tail that stably folds into an alpha helix upon binding to DNA. This recognition helix binds in the minor groove of the DNA. In contrast to other minor-groove binders, it does not induce a sharp bending of the DNA. (PMID 16382145)"))
+saveRDS(a,'./db/tfclass.rds')
 
 # make tree ---------------------------------------------------------------
 # from igraph
@@ -10,7 +11,7 @@ require(igraph)
 require(tidyverse)
 nodes <- a$family%>%mutate(class='family')%>%
   rbind(a$class%>%select(-about)%>%mutate(class='class'))%>%
-  rbind(data.frame(id='2.7',name='C2HC',class='class',stringsAsFactors = F))%>%
+#  rbind(data.frame(id='2.7',name='C2HC',class='class',stringsAsFactors = F))%>%
   rbind(a$superclass%>%select(-about) %>%mutate(class='superclass'))%>%
   rbind(data.frame(id='-',name='root',class='root'))
 edges <- data.frame(from='-',to=nodes%>%filter(class=="superclass")%>%pull(id),stringsAsFactors = F)%>%
@@ -23,11 +24,9 @@ edges <- data.frame(from='-',to=nodes%>%filter(class=="superclass")%>%pull(id),s
                              function(i) data.frame(from=i,to=grep(paste0('^',i,'.'),
                                                                    nodes%>%filter(class=="family")%>%pull(id),value = T)))))
 
-g <- graph_from_data_frame(edges,vertices = nodes)%>% as_tbl_graph()
-
 require(ggraph)
 require(tidygraph)
-
+g <- graph_from_data_frame(edges,vertices = nodes)%>% as_tbl_graph()
 
 
 ggraph(g,'dendrogram',circular=T)+
